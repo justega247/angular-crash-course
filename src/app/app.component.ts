ffeffe81c32faaeb6be1ed5264abfc653901a01f
Tega Okeremeta <oghenetega@Tegas-MacBook-Pro.local>
2019-11-24 00:27:08 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-crash-todolist';
+  name: string = 'Tegas';
+
+  constructor () {
+    this.changeName('Mike')
+  }
+
+  changeName(name:string): void {
+    this.name = name;
+  }
 }
